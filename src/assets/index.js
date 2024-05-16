235f43b0fbf4af539db6c37b96ddcb67edb4bedc
mazhar <swopnil420@gmail.com>
2024-05-17 04:31:23 +0600

    Completed the brand component

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import google from './google.png';
+import slack from './slack.png';
+import atlassian from './atlassian.png';
+import shopify from './shopify.png';
+import dropbox from './dropbox.png';
+
+export { google, slack, atlassian, shopify, dropbox };
